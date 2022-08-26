bd52c5a16cc95da68fb17fc9f7aca1a3d2d4456b
Andre Rousselet <andre098@hotmail.com>
2022-08-26 20:08:48 -0300

    feat: add validation middleware for user creation

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,6 @@
 const { generateToken } = require('./token.middleware');
 const { validateLogin } = require('./login.validator');
+const { validateUser } = require('./addUser.validator');
 const error = require('./error.middleware');
 
-module.exports = { error, generateToken, validateLogin };
+module.exports = { error, generateToken, validateLogin, validateUser };

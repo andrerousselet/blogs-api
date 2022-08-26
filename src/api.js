0a29d020c23aab820efdbb435272d23d75b281ce
Andre Rousselet <andre098@hotmail.com>
2022-08-26 19:20:11 -0300

    feat: add endpoint POST /login

--- src/api.js
+++ src/api.js
@@ -1,12 +1,16 @@
 const express = require('express');
+require('express-async-errors');
 
-// ...
+const controller = require('./controllers');
+const middleware = require('./middlewares');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.post('/login', middleware.validateLogin, controller.login);
+
+app.use(middleware.error);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`

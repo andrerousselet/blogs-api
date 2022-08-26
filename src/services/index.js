b25388af885919aee74d3f402148922a4c6320bd
Andre Rousselet <andre098@hotmail.com>
2022-08-26 19:19:06 -0300

    feat: add login service verifying user in db

---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+const { login } = require('./loginService');
+
+module.exports = { login };
---
+++ src/services/loginService.js
@@ -0,0 +1,11 @@
+const middleware = require('../middlewares/token.middleware');
+const { User } = require('../database/models');
+
+const login = async ({ email, password }) => {
+  const token = middleware.generateToken({ email });
+  const user = await User.findOne({ where: { email, password } });
+  if (!user) return { code: 400, message: 'Invalid fields' };
+  return { code: 200, token };
+};
+
+module.exports = { login };

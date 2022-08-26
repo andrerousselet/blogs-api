bcecad0bf745541c717c5a4f8ff4c8552bda249c
Andre Rousselet <andre098@hotmail.com>
2022-08-26 20:07:37 -0300

    refactor: create message const and simplify error logic

--- src/middlewares/login.validator.js
+++ src/middlewares/login.validator.js
@@ -1,22 +1,23 @@
 const Joi = require('joi');
 
+const MESSAGE = 'Some required fields are missing';
+
 const loginSchema = Joi.object({
   email: Joi.string().email().required().messages({
-    'any.required': '400|Some required fields are missing',
-    'string.empty': '400|Some required fields are missing',
-    'string.email': '422|"email" must be a valid email',
+    'any.required': MESSAGE,
+    'string.empty': MESSAGE,
+    'string.email': '"email" must be a valid email',
   }),
   password: Joi.string().alphanum().required().messages({
-    'any.required': '400|Some required fields are missing',
-    'string.empty': '400|Some required fields are missing',
+    'any.required': MESSAGE,
+    'string.empty': MESSAGE,
   }),
 });
 
 const validateLogin = (req, res, next) => {
   const { error } = loginSchema.validate(req.body);
   if (error) {
-    const [code, message] = error.message.split('|');
-    return res.status(Number(code)).json({ message });
+    return res.status(400).json({ message: error.message });
   }
   next();
 };
